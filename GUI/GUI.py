91fbb643d605fbaef24265469635820fc11969b0
Bob Xu <123419282@qq.com>
2017-02-27 12:35:29 +0800

    latest version
    Mainly change the GUI and euiped with multithread UI design. More
    suitable for batch counting

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -5,6 +5,7 @@ Module implementing HelloWindow.
 """
 import sys, re, os, chardet
 from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog 
 
 from Ui_GUI import Ui_Dialog
@@ -14,6 +15,8 @@ class HelloWindow(QDialog, Ui_Dialog):
     """
     Class documentation goes here.
     """
+    sum=0
+    phsum=0
     def __init__(self, parent=None):
         """
         Constructor
@@ -32,40 +35,71 @@ class HelloWindow(QDialog, Ui_Dialog):
     
     @pyqtSlot()
     def on_probtn_clicked(self):
-        sum=0
+
+        self.thread=Thread(self.directory)
+        self.thread.signal.connect(self.showresult)
+        self.thread.start()
+
+
+    def showresult(self, subsum, phrase, file, amount):
+        self.sum+=subsum
+        self.phsum+=phrase
+        self.textBrowser.append(file)
+        self.textBrowser_2.append(str(subsum))
+        self.textBrowser_3.append(str(phrase))
+        self.label.setText('Total:  '+str(self.sum)+' sentences   '+str(self.phsum//2)+' phrases   '+amount)
+
+class Thread(QThread):
+    
+    signal=pyqtSignal(int, int, str, str)    
+    
+    def __init__(self, directory):
+        self.directory=directory
+        super(Thread, self).__init__()
+        print(self.directory)
+    def run(self):
+        sum=phsum=0
+        print("asd")
         for root, dirs, files in os.walk(self.directory):
+            amount=0
+            current_amount=0
+            file_array=[]
             for file in files:
                 if re.search('.txt$', file):
-                    s=file+':\t'
-                    path=os.path.join(root, file)
-                    file=open(path, 'rb+')
-                    string=file.read()
-                    coding=chardet.detect(string)['encoding']
-                    if coding:
-                        string=string.decode(coding)
-                    else:
-                        string=''
-                    subsum=0
-                    phrase=0
-                    token=0
-                    for word in string:
-                        if re.match(r'[\u3002\uff01\uff1f]', word):
-                            subsum+=1
-                            token=1
-                        elif re.match(r'\n', word):
-                            phrase+=1
-                        if re.match(r'\n', word) and token==1:
+                    amount+=1
+                    file_array.append(file)
+            for file in file_array:
+                file_name=file
+                path=os.path.join(root, file)
+                file=open(path, 'rb+')
+                string=file.read()
+                coding=chardet.detect(string)['encoding']
+                if coding:
+                    string=string.decode(coding)
+                else:
+                    string=''
+                subsum=0
+                phrase=0
+                token=0
+                for word in string:
+                    if re.match(r'[\u3002\uff01\uff1f]', word):
+                        subsum+=1
+                        token=1
+                    elif re.match(r'\n', word):
+                        phrase+=1
+                        if token==1:
                             phrase-=1
                             token=0
-                    
-                    sum+=subsum
-                    s+=str(subsum)+'\t'+str(phrase//2)
+                            
+                current_amount+=1
+                phsum+=phrase
+                sum+=subsum
+                result_amount=str(current_amount)+'/'+str(amount)
+                self.signal.emit(subsum, phrase, file_name, result_amount)
+                print(subsum)
 
-                    self.textBrowser.append(s)
-                    s=''
-        self.label.setText('Total: '+str(sum)+' Chinese sentences\t'+str(phrase//2)+' phrases')
-        
-        
+
+
 if __name__=='__main__':
     app = QApplication(sys.argv)
     dlg=HelloWindow()
